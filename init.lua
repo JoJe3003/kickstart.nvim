aa6a6ae01c46ac3d73d0c3fb1eaf76a519fecce8
JoJe3003 <jfjespersen@gmail.com>
2026-03-18 10:46:04 +0100

    fix: pin blink.cmp to v1.x and add telescope find_files with hidden/ignored

--- init.lua
+++ init.lua
@@ -469,6 +469,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sa', function() builtin.find_files { no_ignore = true, hidden = true } end)
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -793,7 +794,7 @@ require('lazy').setup({
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
-    version = '*',
+    version = '1.*',
     dependencies = {
       -- Snippet Engine
       {
